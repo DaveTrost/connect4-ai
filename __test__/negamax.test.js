a05c420dc54186eb912fbc72dcb949111e8548d8
Dave Trost <davetrost@gmail.com>
2020-01-31 15:04:02 -0800

    test different depths for negamax recursion
    depth of 4 is more accurate but slower (default)
    depth of 3 is fast and useful

--- __test__/negamax.test.js
+++ __test__/negamax.test.js
@@ -4,6 +4,7 @@ const { aiPlays } = require('../lib/solvers/negamax');
 describe('Negamax Solver', () => {
   const width = 7;
   const height = 6;
+  const reducedLookaheadDepth = 3;
 
   it('properly evaluates an immediate winning move in column 1', () => {
     const board = new Board(width, height);
@@ -25,6 +26,18 @@ describe('Negamax Solver', () => {
         33,
       ]
     `);
+
+    expect(aiPlays(board, reducedLookaheadDepth)).toMatchInlineSnapshot(`
+      Array [
+        33,
+        35,
+        33,
+        33,
+        33,
+        33,
+        33,
+      ]
+    `);
   });
 
   it('properly evaluates a winning move in column 1 and a block of the opponents win in column 2', () => {
@@ -37,6 +50,18 @@ describe('Negamax Solver', () => {
     board.play(2);
 
     expect(aiPlays(board)).toMatchInlineSnapshot(`
+      Array [
+        -34,
+        35,
+        33,
+        -34,
+        -34,
+        -34,
+        -34,
+      ]
+    `);
+
+    expect(aiPlays(board, reducedLookaheadDepth)).toMatchInlineSnapshot(`
       Array [
         -34,
         35,
@@ -67,12 +92,36 @@ describe('Negamax Solver', () => {
         33,
       ]
     `);
+
+    expect(aiPlays(board, reducedLookaheadDepth)).toMatchInlineSnapshot(`
+      Array [
+        35,
+        35,
+        0,
+        0,
+        35,
+        35,
+        0,
+      ]
+    `);
   });
 
   it('ranks all columns equally when the board is empty', () => {
     const board = new Board(width, height);
 
     expect(aiPlays(board)).toMatchInlineSnapshot(`
+      Array [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ]
+    `);
+
+    expect(aiPlays(board, reducedLookaheadDepth)).toMatchInlineSnapshot(`
       Array [
         0,
         0,
