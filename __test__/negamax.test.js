d0d82bb1a0d7e08e65c64a02743b75372755fde7
Dave Trost <davetrost@gmail.com>
2020-02-01 23:40:45 -0800

    negamax test modified to align with new AI class

--- __test__/negamax.test.js
+++ __test__/negamax.test.js
@@ -1,5 +1,4 @@
-const { Board } = require('../lib/Board');
-const { negamaxScores } = require('../lib/negamax');
+const Connect4AI = require('../lib/Connect4AI');
 
 describe('Negamax Solver corner cases', () => {
   const width = 7;
@@ -7,9 +6,9 @@ describe('Negamax Solver corner cases', () => {
   const fasterLookaheadDepth = 3;
 
   it('ranks all columns equally when the board is empty', () => {
-    const board = new Board(width, height);
+    const board = new Connect4AI(width, height);
 
-    expect(negamaxScores(board, fasterLookaheadDepth)).toMatchInlineSnapshot(`
+    expect(board.negamaxScores(fasterLookaheadDepth)).toMatchInlineSnapshot(`
       Array [
         0,
         0,
@@ -23,10 +22,10 @@ describe('Negamax Solver corner cases', () => {
   });
 
   it('identifies a column that is full as being unplayable', () => {
-    const board = new Board(width, height);
+    const board = new Connect4AI(width, height);
     board.playMoves([0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 6]);
 
-    expect(negamaxScores(board, fasterLookaheadDepth)[1]).toBe(null);
+    expect(board.negamaxScores(fasterLookaheadDepth)[1]).toBe(null);
   });
 });
 
@@ -36,10 +35,10 @@ describe('Negamax Solver with default/faster settings', () => {
   const fasterLookaheadDepth = 3;
 
   it('identifies an immediate winning move in column 1', () => {
-    const board = new Board(width, height);
+    const board = new Connect4AI(width, height);
     board.playMoves([1, 2, 1, 4, 1, 6]);
 
-    expect(negamaxScores(board, fasterLookaheadDepth)).toMatchInlineSnapshot(`
+    expect(board.negamaxScores(fasterLookaheadDepth)).toMatchInlineSnapshot(`
       Array [
         0,
         35,
@@ -53,10 +52,10 @@ describe('Negamax Solver with default/faster settings', () => {
   });
 
   it('identifies a winning move in column 1 and a block of the opponents win in column 2', () => {
-    const board = new Board(width, height);
+    const board = new Connect4AI(width, height);
     board.playMoves([1, 2, 1, 2, 1, 2]);
 
-    expect(negamaxScores(board, fasterLookaheadDepth)).toMatchInlineSnapshot(`
+    expect(board.negamaxScores(fasterLookaheadDepth)).toMatchInlineSnapshot(`
       Array [
         -34,
         35,
@@ -70,10 +69,10 @@ describe('Negamax Solver with default/faster settings', () => {
   });
 
   it('identifies moves at columns 1 and 4 that each result in a win, 3 plays into the future', () => {
-    const board = new Board(width, height);
+    const board = new Connect4AI(width, height);
     board.playMoves([2, 2, 3, 3]);
 
-    expect(negamaxScores(board, fasterLookaheadDepth)).toMatchInlineSnapshot(`
+    expect(board.negamaxScores(fasterLookaheadDepth)).toMatchInlineSnapshot(`
       Array [
         0,
         35,
@@ -93,10 +92,10 @@ describe.skip('Negamax Solver with harder/slower settings', () => {
   const slowerLookaheadDepth = 4;
 
   it('identifies an immediate winning move in column 1', () => {
-    const board = new Board(width, height);
+    const board = new Connect4AI(width, height);
     board.playMoves([1, 2, 1, 4, 1, 6]);
 
-    expect(negamaxScores(board, slowerLookaheadDepth)).toMatchInlineSnapshot(`
+    expect(board.negamaxScores(slowerLookaheadDepth)).toMatchInlineSnapshot(`
       Array [
         0,
         35,
@@ -110,10 +109,10 @@ describe.skip('Negamax Solver with harder/slower settings', () => {
   });
 
   it('identifies a winning move in column 1 and a block of the opponents win in column 2', () => {
-    const board = new Board(width, height);
+    const board = new Connect4AI(width, height);
     board.playMoves([1, 2, 1, 2, 1, 2]);
 
-    expect(negamaxScores(board, slowerLookaheadDepth)).toMatchInlineSnapshot(`
+    expect(board.negamaxScores(slowerLookaheadDepth)).toMatchInlineSnapshot(`
       Array [
         -34,
         35,
@@ -127,10 +126,10 @@ describe.skip('Negamax Solver with harder/slower settings', () => {
   });
 
   it('finds moves at columns 1 and 4 that each result in a win, 3 plays into the future', () => {
-    const board = new Board(width, height);
+    const board = new Connect4AI(width, height);
     board.playMoves([2, 2, 3, 3]);
 
-    expect(negamaxScores(board, slowerLookaheadDepth)).toMatchInlineSnapshot(`
+    expect(board.negamaxScores(slowerLookaheadDepth)).toMatchInlineSnapshot(`
       Array [
         0,
         35,
@@ -141,12 +140,5 @@ describe.skip('Negamax Solver with harder/slower settings', () => {
         0,
       ]
     `);
-  });
-
-  it('plays defensively', () => {
-    const board = new Board(width, height);
-    board.playMoves([2, 3, 1, 2, 1, 3, 0, 4, 0, 5, 5, 6, 2, 6, 1, 1]);
-
-    expect(negamaxScores(board, slowerLookaheadDepth)).toMatchInlineSnapshot();
   });
 });
