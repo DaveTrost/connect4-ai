0034f5bd6ebfea74753e7ca6b17db989d271a556
Dave Trost <davetrost@gmail.com>
2020-02-01 22:50:02 -0800

    passing tests again (after major code reorg)

--- __test__/board.test.js
+++ __test__/board.test.js
@@ -1,11 +1,11 @@
-const { Board } = require('../lib/Board');
+const Connect4 = require('../lib/Connect4');
 
-describe('Board Class', () => {
+describe('Connect4 Class', () => {
   const width = 7;
   const height = 6;
 
-  it('uses default board size if not provided', () => {
-    const board = new Board();
+  it('uses default size if not provided', () => {
+    const board = new Connect4();
 
     expect(board.ascii()).toMatchInlineSnapshot(`
       "
@@ -21,7 +21,7 @@ describe('Board Class', () => {
   });
 
   it('has basic game play functionality', () => {
-    const board = new Board(width, height);
+    const board = new Connect4(width, height);
 
     expect(board.ascii()).toMatchInlineSnapshot(`
       "
@@ -35,7 +35,7 @@ describe('Board Class', () => {
       [0][1][2][3][4][5][6]"
     `);
     expect(board.getActivePlayer()).toBe(1);
-    expect(board.getMoves()).toBe(0);
+    expect(board.getMoveCount()).toBe(0);
     expect(board.gameOver).toBeFalsy();
     expect(board.winner).toBeNull;
     expect(board.gameStatus()).toMatchInlineSnapshot(`
@@ -76,7 +76,7 @@ describe('Board Class', () => {
   });
 
   it('can predict a winning move in a vertical direction', () => {
-    const board = new Board(width, height);
+    const board = new Connect4(width, height);
     board.play(1);
     board.play(2);
     board.play(1);
@@ -99,7 +99,7 @@ describe('Board Class', () => {
   });
 
   it('can predict winning moves in a horizontal direction', () => {
-    const board = new Board(width, height);
+    const board = new Connect4(width, height);
     board.play(1);
     board.play(6);
     board.play(2);
@@ -120,7 +120,7 @@ describe('Board Class', () => {
     `);
     expect(board.isWinningMove(4)).toBeTruthy();
 
-    const board2 = new Board(width, height);
+    const board2 = new Connect4(width, height);
     board2.play(1);
     board2.play(6);
     board2.play(2);
@@ -143,7 +143,7 @@ describe('Board Class', () => {
   });
 
   it('can predict winning moves in both diagonal directions', () => {
-    const board = new Board(width, height);
+    const board = new Connect4(width, height);
     board.play(0);
     board.play(6);
     board.play(5);
@@ -187,7 +187,7 @@ describe('Board Class', () => {
     `);
     expect(board.isWinningMove(3)).toBeTruthy();
 
-    const board2 = new Board(width, height);
+    const board2 = new Connect4(width, height);
     board2.play(0);
     board2.play(2);
     board2.play(1);
@@ -239,7 +239,7 @@ describe('Board Class', () => {
   });
 
   it('can predict winning moves for corner cases', () => {
-    const board = new Board(width, height);
+    const board = new Connect4(width, height);
     const plays = '312033242241434434636255'.split('');
     plays.forEach(play => board.play(play));
 
@@ -258,38 +258,38 @@ describe('Board Class', () => {
   });
 
   it('can make plays with 1-based indices', () => {
-    const board = new Board(width, height);
+    const board = new Connect4(width, height);
     board.play1BasedColumn(1);
     board.play1BasedColumn(2);
     expect(board.getActivePlayer()).toBe(1);
-    expect(board.getMoves()).toBe(2);
+    expect(board.getMoveCount()).toBe(2);
     expect(board.gameOver).toBeFalsy();
   });
 
   it('can work with boards of different sizes', () => {
     const size = 3;
-    const board = new Board(size, size);
+    const board = new Connect4(size, size);
     board.play(0);
     board.play(2);
     board.play(1);
-    expect(board.getMoves()).toBe(3);
+    expect(board.getMoveCount()).toBe(3);
   });
 
   it('catches invalid moves', () => {
     const size = 3;
-    const board = new Board(size, size);
+    const board = new Connect4(size, size);
 
     expect(board.canPlay(-1)).toBeFalsy();
     expect(board.canPlay(3)).toBeFalsy();
     expect(() => board.play(-1)).toThrow();
     expect(() => board.play(3)).toThrow();
-    expect(board.getMoves()).toBe(0);
+    expect(board.getMoveCount()).toBe(0);
   });
 
   it('ends the game when a connect-4 is found', () => {
     const width = 4;
     const height = 3;
-    const board = new Board(width, height);
+    const board = new Connect4(width, height);
 
     board.play(0);
     board.play(0);
@@ -307,15 +307,15 @@ describe('Board Class', () => {
       }
     `);
 
-    expect(board.getMoves()).toBe(7);
+    expect(board.getMoveCount()).toBe(7);
     for(let i = 0; i < 10; i++) board.play(3);
-    expect(board.getMoves()).toBe(7);
+    expect(board.getMoveCount()).toBe(7);
   });
 
   it('ends the game when the board is full', () => {
     const width = 2;
     const height = 1;
-    const board = new Board(width, height);
+    const board = new Connect4(width, height);
 
     board.play(0);
     board.play(1);
@@ -328,8 +328,8 @@ describe('Board Class', () => {
       }
     `);
 
-    expect(board.getMoves()).toBe(2);
+    expect(board.getMoveCount()).toBe(2);
     for(let i = 0; i < 10; i++) board.play(0);
-    expect(board.getMoves()).toBe(2);
+    expect(board.getMoveCount()).toBe(2);
   });
 });
