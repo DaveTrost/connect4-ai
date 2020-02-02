e000583278a32a134fce40e5154627f65f2603d3
Dave Trost <davetrost@gmail.com>
2020-02-01 23:43:15 -0800

    all tests working

--- __test__/aiPlay.test.js
+++ __test__/aiPlay.test.js
@@ -1,22 +1,22 @@
-const { Board } = require('../lib/Board');
-const { aiPlay, getTargetScore } = require('../lib/aiPlay');
+const { getTargetScore } = require('../lib/helperFunctions');
+const Connect4AI = require('../lib/Connect4AI');
 
 describe('AI Game Play', () => {
   const width = 7;
   const height = 6;
 
   it('selects the winning move in column 1 using "hard" difficulty', () => {
-    const board = new Board(width, height);
+    const board = new Connect4AI(width, height);
     board.playMoves([1, 2, 1, 2, 1, 2]);
-    const computerMove = aiPlay(board, 'hard');
+    const computerMove = board.playAI('hard');
 
     expect(computerMove).toBe(1);
   });
 
   it('selects either the winning move in column 1 or the blocking move in column 2 using default difficulty', () => {
-    const board = new Board(width, height);
+    const board = new Connect4AI(width, height);
     board.playMoves([1, 2, 1, 2, 1, 2]);
-    const computerMove = aiPlay(board);
+    const computerMove = board.playAI();
 
     expect(computerMove).toBeGreaterThanOrEqual(1);
     expect(computerMove).toBeLessThanOrEqual(2);
